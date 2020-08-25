124afd021d9f26c6d436bdd5db29d5db89958ef7
JackyLIM <jacky.lim@edu.itescia.fr>
2020-08-25 23:17:12 +0200

    Feat: Use DiscountOffer class property to update fields

--- entities/Store.js
+++ entities/Store.js
@@ -17,52 +17,46 @@ module.exports = class Store {
       ) {
         if (this.discountOffers[i].discountInPercent > 0) {
           if (this.discountOffers[i].partnerName != "Ilek") {
-            /*this.discountOffers[i].discountInPercent =
-              this.discountOffers[i].discountInPercent - 1;*/
             this.discountOffers[i].updateProperty("discountInPercent", -1);
           }
         }
       } else {
         if (this.discountOffers[i].discountInPercent < 50) {
-          this.discountOffers[i].discountInPercent =
-            this.discountOffers[i].discountInPercent + 1;
+          this.discountOffers[i].updateProperty("discountInPercent", 1);
           if (this.discountOffers[i].partnerName == "Vinted") {
             if (this.discountOffers[i].expiresIn < 11) {
               if (this.discountOffers[i].discountInPercent < 50) {
-                this.discountOffers[i].discountInPercent =
-                  this.discountOffers[i].discountInPercent + 1;
+                this.discountOffers[i].updateProperty("discountInPercent", 1);
               }
             }
             if (this.discountOffers[i].expiresIn < 6) {
               if (this.discountOffers[i].discountInPercent < 50) {
-                this.discountOffers[i].discountInPercent =
-                  this.discountOffers[i].discountInPercent + 1;
+                this.discountOffers[i].updateProperty("discountInPercent", 1);
               }
             }
           }
         }
       }
       if (this.discountOffers[i].partnerName != "Ilek") {
-        this.discountOffers[i].expiresIn = this.discountOffers[i].expiresIn - 1;
+        this.discountOffers[i].updateProperty("expiresIn", -1);
       }
       if (this.discountOffers[i].expiresIn < 0) {
         if (this.discountOffers[i].partnerName != "Naturalia") {
           if (this.discountOffers[i].partnerName != "Vinted") {
             if (this.discountOffers[i].discountInPercent > 0) {
               if (this.discountOffers[i].partnerName != "Ilek") {
-                this.discountOffers[i].discountInPercent =
-                  this.discountOffers[i].discountInPercent - 1;
+                this.discountOffers[i].updateProperty("discountInPercent", 1);
               }
             }
           } else {
-            this.discountOffers[i].discountInPercent =
-              this.discountOffers[i].discountInPercent -
-              this.discountOffers[i].discountInPercent;
+            this.discountOffers[i].updateProperty(
+              "discountInPercent",
+              -this.discountOffers[i].discountInPercent
+            );
           }
         } else {
           if (this.discountOffers[i].discountInPercent < 50) {
-            this.discountOffers[i].discountInPercent =
-              this.discountOffers[i].discountInPercent + 1;
+            this.discountOffers[i].updateProperty("discountInPercent", 1);
           }
         }
       }
