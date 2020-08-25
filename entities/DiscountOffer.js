3f71faadb77b77978596fc9a90b8af72bc368f8c
JackyLIM <jacky.lim@edu.itescia.fr>
2020-08-25 23:00:06 +0200

    Feat: Add class method to update its property

--- entities/DiscountOffer.js
+++ entities/DiscountOffer.js
@@ -9,4 +9,13 @@ export class DiscountOffer {
     this.expiresIn = expiresIn;
     this.discountInPercent = discountRateInPercent;
   }
+
+  /**
+   * Update any property with the given value
+   * @param {string} propertyName - Name of the property to update
+   * @param {number} value - Value to add or retrieve to the property
+   */
+  updateProperty(propertyName, value) {
+    this[propertyName] = this[propertyName] + value;
+  }
 }
