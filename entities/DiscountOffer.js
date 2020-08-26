ccb0324f40120c522feb0f783ede3f14ab29130f
JackyLIM <jacky.lim@edu.itescia.fr>
2020-08-26 22:56:27 +0200

    Feat: Add parameter to updateDiscount method to decrease the expiration value

--- entities/DiscountOffer.js
+++ entities/DiscountOffer.js
@@ -14,9 +14,10 @@ module.exports = class DiscountOffer {
    * Update the discount rate given the value in parameter
    * And decrease the expiresIn value by 1 every day (unless it is already at 0)
    * @param {number} value - Value to add or retrieve to the property
+   * @param {boolean} expireDecrease - True if expiresIn property can decrease
    */
-  updateDiscount(value) {
-    if (this.expiresIn > 0) {
+  updateDiscount(value, expireDecrease = true) {
+    if (this.expiresIn > 0 && expireDecrease) {
       this.expiresIn = this.expiresIn - 1; // ExpiresIn property decrease every day
     }
     // DiscountInPercent can't excess 50
