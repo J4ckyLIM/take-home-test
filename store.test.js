0deb03c2ac8f2f625f6da14907081beb5756077d
JackyLIM <jacky.lim@edu.itescia.fr>
2020-08-26 23:12:22 +0200

    Feat: Add test for Ilek and Vinted Cases

--- store.test.js
+++ store.test.js
@@ -7,4 +7,19 @@ describe("Store", () => {
       new Store([new DiscountOffer("test", 2, 3)]).updateDiscounts()
     ).toEqual([new DiscountOffer("test", 1, 2)]);
   });
+  it("should not modify any value", () => {
+    expect(
+      new Store([new DiscountOffer("Ilek", 12, 32)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("Ilek", 12, 32)]);
+  });
+  it("should have discount increased by 3 and expiresIn decreased by 1", () => {
+    expect(
+      new Store([new DiscountOffer("Vinted", 4, 25)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("Vinted", 3, 28)]);
+  });
+  it("should have discount increased by 2 and expiresIn decreased by 1", () => {
+    expect(
+      new Store([new DiscountOffer("Vinted", 8, 30)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("Vinted", 7, 32)]);
+  });
 });
